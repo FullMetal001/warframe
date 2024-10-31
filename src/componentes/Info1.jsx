d4c869e8c81ea90e697cb1879bc8610faae45284
github_FullMetal001 <tantun_gel@hotmail.com>
2024-10-31 11:45:50 -0400

    cambios

--- src/componentes/Info1.jsx
+++ src/componentes/Info1.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Info1 = () => {
   return (
     <div className='info1-container'>
-        <h2>¡DOMINATES THE GAME!</h2> 
+        <h2 className='dominate'>DOMINATES</h2> 
+        <h2 className='the-game'>THE GAME</h2>
         <p>Join our community and discover everything you need to become a master. Guides, tips, tricks, advice and more to improve your gaming experience.</p>   
     </div>
   )
